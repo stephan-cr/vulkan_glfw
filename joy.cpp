5b70b9dc95d9bad6bf754ac4bb9e9614e07e45ba
Stephan Creutz <stephan.cr@gmx.de>
2024-02-04 09:20:20 +0100

    add mini example to test the gamepad

---
+++ joy.cpp
@@ -0,0 +1,125 @@
+#include "GLFW/glfw3.h"
+
+#include <cmath>
+#include <ios>
+#include <iostream>
+#include <ostream>
+#include <utility>
+
+void joystick_callback(int jid, int event)
+{
+  std::cout << "joystick event\n";
+  if (event == GLFW_CONNECTED) {
+    // The joystick was connected
+
+    std::cout << "joystick connected\n";
+  }
+  else if (event == GLFW_DISCONNECTED) {
+    // The joystick was disconnected
+
+    std::cout << "joystick disconnected\n";
+  }
+
+  std::cout << std::endl;
+}
+
+struct Pos
+{
+  float x;
+  float y;
+};
+
+std::ostream& operator<< (std::ostream& stream, const Pos& pos)
+{
+  stream << "(" << pos.x << ", " << pos.y << ")";
+  return stream;
+}
+
+int main()
+{
+  if (!glfwInit()) {
+    // Initialization failed
+  }
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", nullptr, nullptr);
+  if (!window) {
+    // Window or context creation failed
+  }
+
+  glfwSetJoystickCallback(joystick_callback);
+  glfwMakeContextCurrent(window);
+
+  std::cout << std::boolalpha;
+  for (int i = 0; i < GLFW_JOYSTICK_LAST; i++) {
+    std::cout << "joy: " << (glfwJoystickPresent(i) == GLFW_TRUE) << '\n';
+  }
+
+  std::cout << "is gamepad: " << glfwJoystickIsGamepad(GLFW_JOYSTICK_1) << '\n' <<
+    "gamepad name: " << glfwGetGamepadName(GLFW_JOYSTICK_1) << '\n';
+
+  GLFWgamepadstate state;
+  Pos axis_left_last_pos = { 0, 0 };
+  Pos axis_right_last_pos = { 0, 0 };
+  while (!glfwWindowShouldClose(window)) {
+    // Keep running
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    if (glfwGetGamepadState(GLFW_JOYSTICK_1, &state)) {
+      if (state.buttons[GLFW_GAMEPAD_BUTTON_A]) {
+        std::cout << "gamepad button A\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_B]) {
+        std::cout << "gamepad button B\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_X]) {
+        std::cout << "gamepad button X\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_Y]) {
+        std::cout << "gamepad button Y\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_LEFT_BUMPER]) {
+        std::cout << "gamepad left bumper\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER]) {
+        std::cout << "gamepad right bumper\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_GUIDE]) {
+        std::cout << "gamepad guide\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_START]) {
+        std::cout << "gamepad start\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_BACK]) {
+        std::cout << "gamepad back\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_LEFT_THUMB]) {
+        std::cout << "gamepad left thumb\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_THUMB]) {
+        std::cout << "gamepad right thumb\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP]) {
+        std::cout << "gamepad dpad up\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT]) {
+        std::cout << "gamepad dpad right\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN]) {
+        std::cout << "gamepad dpad down\n";
+      } else if (state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT]) {
+        std::cout << "gamepad dpad left\n";
+      }
+
+#if 0
+      std::cout << "left/right trigger: " << state.axes[GLFW_GAMEPAD_AXIS_LEFT_TRIGGER] << ", "
+                << state.axes[GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER] << '\n';
+#endif
+
+      Pos axis_left_current_pos = { state.axes[GLFW_GAMEPAD_AXIS_LEFT_X], state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y] };
+      Pos axis_right_current_pos = { state.axes[GLFW_GAMEPAD_AXIS_RIGHT_X], state.axes[GLFW_GAMEPAD_AXIS_RIGHT_Y] };
+
+      if (std::fabs(axis_left_current_pos.x - axis_left_last_pos.x) >= 0.1 ||
+          std::fabs(axis_left_current_pos.y - axis_left_last_pos.y) >= 0.1) {
+        std::cout << "gamepad left: " << axis_left_current_pos << '\n';
+      }
+
+      if (std::fabs(axis_right_current_pos.x - axis_right_last_pos.x) >= 0.1 ||
+          std::fabs(axis_right_current_pos.y - axis_right_last_pos.y) >= 0.1) {
+        std::cout << "gamepad right: " << axis_right_current_pos << '\n';
+      }
+    }
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
